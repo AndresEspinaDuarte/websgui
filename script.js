8885c82ad51df907811332e6e640ede2870bb1c2
Andres Espina Duarte <36141101+AndresEspinaDuarte@users.noreply.github.com>
2020-08-20 12:11:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+(function($) {
+  
+  'use strict';
+
+  // variables
+  var $isAnimatedFirst = $('.first .is-animated'),
+      $isAnimatedSecond = $('.second .is-animated'),
+      $isAnimatedSecondSingle = $('.second .is-animated__single'),
+      $isAnimatedThird = $('.third .is-animated'),
+      $isAnimatedThirdSingle = $('.third .is-animated__single'),
+      $isAnimatedFourth = $('.fourth .is-animated'),
+      $isAnimatedFive = $('.five .is-animated'),
+      $isAnimatedFourthSingle = $('.fourth .is-animated__single');
+
+  // initialize fullPage
+  $('#fullpage').fullpage({
+
+    navigation: true,
+    onLeave: function(index, nextIndex, direction) {
+    
+      /**
+      * use the following condition: 
+      *
+      *   if( index == 1 && direction == 'down' ) {
+      *
+      * if you haven't enabled the dot navigation
+      * or you aren't interested in the animations that occur 
+      * when you jump (using the dot navigation) 
+      * from the first section to another sections 
+      */
+      
+      // first animation
+      if( index == 1 && nextIndex == 2 ) { 
+        $isAnimatedSecond.addClass('animated fadeInUpBig'); 
+        $isAnimatedSecond.css('animation-delay', '.333s');
+      }
+
+    /**
+      * use the following condition: 
+      *
+      *   else if( index == 2 && direction == 'down' ) {
+      *
+      * if you haven't enabled the dot navigation
+      * or you aren't interested in the animations that occur 
+      * when you jump (using the dot navigation) from the first section to the third one 
+      */
+      
+      // second animation
+      else if( ( index == 1 || index == 2 ) && nextIndex == 3 ) {
+        $isAnimatedThird.addClass('animated fadeInUpBig'); 
+        $isAnimatedThird.css('animation-delay', '.333s');
+      }
+
+      
+     /**
+      * use the following condition:
+      *
+      *   else if( index == 3 && direction == 'down' ) {
+      *
+      * if you haven't enabled the dot navigation
+      * or you aren't interested in the animations that occur 
+      * when you jump (using the dot navigation) 
+      * from the first or second section to the fourth one 
+      */
+      
+      // third animation
+      else if( ( index == 1 || index == 2 || index == 3 ) && nextIndex == 4 ) {
+        $isAnimatedFourth.addClass('animated fadeInUpBig'); 
+        $isAnimatedFourth.css('animation-delay', '.333s');
+      }
+
+      else if( ( index == 1 || index == 2 || index == 3 || index == 4 ) && nextIndex == 5 ) {
+        $isAnimatedFive.addClass('animated fadeInUpBig'); 
+        $isAnimatedFive.css('animation-delay', '.333s');
+      }
+    }
+
+  });
+  
+})(jQuery);
